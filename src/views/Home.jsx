e1642b53d76d6bed5047baa87a385bb5486a91d5
Agustin <aggarcia997@gmail.com>
2024-10-28 07:54:26 -0300

    Ubicacion de componentes del Home

---
+++ src/views/Home.jsx
@@ -0,0 +1,23 @@
+import {Container} from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import {Banner} from "../components/Home/banner/Banner.jsx";
+import {Recomendations} from "../components/Home/recomendations/Recomendations.jsx";
+import {Resources} from "../components/Home/resources/Resources.jsx";
+
+export const Home = () => {
+    return(
+
+            <Grid container sx={{width:'90%', display:'flex', justifyContent:'center', alignItems:'center'}}>
+                <Grid item sx={{width:'100%'}}>
+                    <Banner/>
+                </Grid>
+                <Grid item sx={{width:'100%',display:'flex', justifyContent:'center', alignItems:'center',  backgroundColor:'#fff'}}>
+                    <Recomendations/>
+                </Grid>
+                <Grid item sx={{width:'100%'}}>
+                    <Resources/>
+                </Grid>
+            </Grid>
+
+    )
+}
