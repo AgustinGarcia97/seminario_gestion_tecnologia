2ee532731a9286a59037ac69d24ab18ad3f4aaa3
Agustin <aggarcia997@gmail.com>
2024-10-28 07:52:18 -0300

    Reajuste del enrutado de la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import {Navbar} from "./components/Navbar/Navbar.jsx";
+import {login_style} from "./views/Login.jsx";
+import {Title} from "./components/app/Title.jsx";
+import {Container} from "@mui/material";
+import {ModuleRoutes} from "./routes/Routes.jsx";
+import {BrowserRouter} from "react-router-dom";
 
 
 function App() {
 
 
   return (
-    <>
-    </>
+      <BrowserRouter>
+        <Container maxWidth={1} sx={{...login_style.container,flexDirection:'column',}}>
+            <Title/>
+            <Navbar></Navbar>
+            <ModuleRoutes/>
+        </Container>
+      </BrowserRouter>
   )
 }
 
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,13 +1,18 @@
-import {createBrowserRouter} from "react-router-dom";
+
+import {Route, Routes} from "react-router-dom";
+import {Home} from "../views/Home.jsx";
+
 import {Login} from "../views/Login.jsx";
+import {Register} from "../views/Register.jsx";
+import {RegisterLastStep} from "../components/Register/RegisterLastStep.jsx";
 
-
-export const router = createBrowserRouter([
-
-    {
-        path:'/login',
-        element: <Login/>
-    }
-
-
-    ]);
+export const ModuleRoutes = () => {
+    return (
+        <Routes>
+            <Route path={"/"} element={<Home/>}/>
+            <Route path={"/login"} element={<Login/>}/>
+            <Route path={"/register"} element={<Register/>}/>
+            <Route path={"/next-step-register"} element={<RegisterLastStep/>}/>
+        </Routes>
+    )
+}
