1346257455eb2ccf4611b5e2a2f01e355bee4088
Agustin <aggarcia997@gmail.com>
2024-10-28 07:53:25 -0300

    titulo de cada app

---
+++ src/components/app/Title.jsx
@@ -0,0 +1,14 @@
+import {Typography} from "@mui/material";
+
+export const Title = () => {
+    return(
+        <Typography sx={{
+            color:'#9b9898',
+            fontSize:{xs:'',md:'',lg:'',xl:45},
+            fontWeight:'bold',
+            width:'90%',
+            boxSizing:'border-box',
+
+        }}>Login</Typography>
+    )
+}
