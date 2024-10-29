bb6f5fa43102473ee873f5fb9fdbe9bc69e6bc11
Agustin <aggarcia997@gmail.com>
2024-10-29 03:21:37 -0300

    Implementacion de vista Progress en proceso hasta definicion en canva

---
+++ src/views/Progress.jsx
@@ -0,0 +1,9 @@
+import {Box, Typography} from "@mui/material";
+
+export const Progress = () => {
+    return(
+        <Box sx={{height:'75vh', display:'flex', justifyContent:'center',alignItems:'center' }}>
+            <Typography variant="h3" component="div">Próximamente...</Typography>
+        </Box>
+    )
+}
